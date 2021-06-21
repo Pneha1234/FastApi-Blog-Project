dd08f24c223884ffadcd04ec0be369cb1bb750cf
Pneha1234 <nehapandey408@gmail.com>
2021-06-21 08:13:58 +0545

    created models and blogs

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 from blog import models
-from blog.database import engine
+from blog.database import engine, SessionLocal
 from blog.schemas import Blog
 
 app = FastAPI()
 models.Base.metadata.create_all(engine)
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post('/blog')
-def create(request: Blog):
-    return request
-    return 'creating blogs'
+def create(request: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
